aedad364eca2d58bc79932532897a4ef4b38972b
Ben Weitzman <benweitzman@gmail.com>
2013-08-29 17:03:59 -0400

    added functionality to remove windows and rotate splits

--- XMonad/Layout/BinarySpacePartition.hs
+++ XMonad/Layout/BinarySpacePartition.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, DeriveDataTypeable #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  XMonad.Layout.BinarySpacePartition
@@ -13,13 +13,18 @@
 --
 -----------------------------------------------------------------------------
  
-module XMonad.Layout.BinarySpacePartition (BinarySpacePartition(..), BSP(..)) where
+module XMonad.Layout.BinarySpacePartition (BinarySpacePartition(..), BSP(..), Rotate(..)) where
 
+import XMonad
 import XMonad.Core
-import Graphics.X11 (Rectangle(..))
 import qualified XMonad.StackSet as W
 import XMonad.Util.Stack
+import qualified Data.Map as M
+import Data.List ((\\))
+import Control.Monad
 
+data Rotate = Rotate deriving Typeable
+instance Message Rotate
 
 data Direction = Horizontal | Vertical deriving (Show, Read)
 
@@ -36,19 +41,32 @@ data BSP = EmptyBSP | Leaf | Split { left :: BSP
 data BinarySpacePartition a = BinarySpacePartition BSP deriving (Show, Read)
 
 instance LayoutClass BinarySpacePartition a where
-    doLayout (BinarySpacePartition bsp) r s = return (zip ws rs, layout) where
-        ws = W.integrate s
-        count = size bsp
-        layout = if length ws == count then Nothing 
-                 else Just (BinarySpacePartition (splitNth bsp n)) where
-                        n = case toIndex (Just s) of
-                              (_, Nothing) -> 0
-                              (_, Just int) -> int
-        rs = case layout of
-                Nothing -> rectangles bsp r
-                (Just (BinarySpacePartition bsp')) -> rectangles bsp' r
-    pureMessage _ _ = Nothing
-    description _  = "BSP"
+  doLayout cur@(BinarySpacePartition bsp) r s = return (zip ws rs, layout) where
+    ws = W.integrate s
+    count = size bsp
+    layout = if l == count then Just (cur)
+             else if l > count then Just (BinarySpacePartition (splitNth bsp n))
+                  else  Just (BinarySpacePartition (removeNth bsp n))
+    l = length ws
+    n = case toIndex (Just s) of
+      (_, Nothing) -> 0
+      (_, Just int) -> int
+    rs = case layout of
+      Nothing -> rectangles bsp r
+      (Just (BinarySpacePartition bsp')) -> rectangles bsp' r
+  handleMessage (BinarySpacePartition bsp) m =
+    do ms <- (W.stack . W.workspace . W.current) `fmap` gets windowset
+       fs <- (M.keys . W.floating) `fmap` gets windowset
+       return $ ms >>= unfloat fs >>= handleMesg
+    where handleMesg s = msum [fmap (\x -> rotate x s) (fromMessage m)]
+          unfloat fs s = if W.focus s `elem` fs
+                         then Nothing
+                         else Just (s { W.up = (W.up s) \\ fs
+                                      , W.down = (W.down s) \\ fs })
+          rotate Rotate s = BinarySpacePartition (rotateNth bsp (case toIndex (Just s) of
+                                                                    (_, Nothing) -> 0
+                                                                    (_, Just int) -> int))
+  description _  = "BSP"
 
 split :: Direction -> Rational -> Rectangle -> (Rectangle, Rectangle)
 split Horizontal r (Rectangle sx sy sw sh) = (r1, r2) where 
@@ -66,6 +84,15 @@ rectangles Leaf rootRect = [rootRect]
 rectangles (Split l r d ra) rootRect = 
     rectangles l leftBox ++ rectangles r rightBox where
     (leftBox, rightBox) = split d ra rootRect
+    
+rightLeaf :: BSP -> Int -> Bool
+rightLeaf EmptyBSP _ = False
+rightLeaf (Split Leaf _ _ _) 0 = False
+rightLeaf (Split l Leaf _ _) n | size l <= n = True
+rightLeaf Leaf _ = True
+rightLeaf (Split l r _ _) n = if size l > n
+                              then rightLeaf l n
+                              else rightLeaf r (n - size l)
 
 rightMostSplit :: BSP -> BSP
 rightMostSplit EmptyBSP = Leaf
@@ -78,11 +105,33 @@ splitNth :: BSP -> Int -> BSP
 splitNth EmptyBSP _ = Leaf
 splitNth (Split Leaf r d ra) 0 = Split (Split Leaf Leaf (opposite d) 0.5) r d ra
 splitNth (Split l Leaf d ra) n
-    | size l <= n = Split l (Split Leaf Leaf (opposite d) 0.5) d ra
+  | size l <= n = Split l (Split Leaf Leaf (opposite d) 0.5) d ra
 splitNth Leaf _ = Split Leaf Leaf Vertical 0.5
 splitNth (Split l r d ra) n = if size l >  n 
                               then Split (splitNth l n) r d ra
                               else Split l (splitNth r (n - size l)) d ra
+
+removeNth :: BSP -> Int -> BSP
+removeNth EmptyBSP _ = EmptyBSP
+removeNth (Split Leaf r _ _) 0 = r
+removeNth (Split l Leaf _ _) n
+  | size l <= n = l
+removeNth Leaf _ = EmptyBSP
+removeNth (Split l r d ra) n = if size l > n
+                               then Split (removeNth l n) r d ra
+                               else Split l (removeNth r (n - size l)) d ra
+
+rotateNth :: BSP -> Int -> BSP
+rotateNth EmptyBSP _ = EmptyBSP
+rotateNth (Split Leaf r d ra) 0 = Split Leaf r (opposite d) ra
+rotateNth (Split l Leaf d ra) n
+  | size l <= n = Split l Leaf (opposite d) ra
+rotateNth Leaf _ = Leaf
+rotateNth (Split l r d ra) n = if size l > n
+                               then Split (rotateNth l n) r d ra
+                               else Split l (rotateNth r (n - size l)) d ra
+
+
 
 size :: BSP -> Int
 size EmptyBSP = 0
